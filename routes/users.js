55dc4e51222b8f2bfca8168786d0b88fbee9dfe8
coolicedan1 <coolicedan@gmail.com>
2023-07-27 12:41:15 +0100

    add users/delete user

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 
 // UPDATE USER 
 router.put("/:id", async(req, res)=>{
-  if(req.body.userId === req.params.id || req.user.isAdmin){
+  if(req.body.userId === req.params.id || req.body.isAdmin){
       if(req.body.password){
         try{
           const salt = await bcrypt.genSalt(10);
@@ -24,9 +24,24 @@ router.put("/:id", async(req, res)=>{
   }else {
     return res.status(403).json("you can update only your account")
   }
-})
+});
+
 // DELETE A USER
+router.delete("/:id", async(req, res)=>{
+  if(req.body.userId === req.params.id || req.body.isAdmin){
+      try{
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("Account has been deleted");
+      }catch(err){
+        return res.status(500).json(err);
+      }
+  }else {
+    return res.status(403).json("you can delete only your account")
+  }
+});
+
 // GET A USER
+
 // FOLLOW A USERS
 // UNFOLLOW A USER
 
